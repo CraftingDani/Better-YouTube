d5488cdd5823ca0ad49734e3fbb9f31935807b99
CraftingDani <daniel.gleich2007@gmail.com>
2022-02-01 20:19:52 +0100

    add /watch & customizable video count

---
+++ watch/script.js
@@ -0,0 +1,14 @@
+//-- Constants --\\
+
+const vid = document.querySelector("[data-vidTemplate]")
+const otherVidsSection = document.querySelector(".otherVids")
+
+const vidCount = 10
+
+
+//-- features --\\
+
+for (let i = 0; i < vidCount; i++)
+{
+    otherVidsSection.appendChild(vid.content.cloneNode(true))
+}
