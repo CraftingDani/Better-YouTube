d02f9aa3b2f87f696f2ae1778e6a838c009b3ee6
CraftingDani <daniel.gleich2007@gmail.com>
2022-02-01 20:25:20 +0100

    add /watch & customizable video count

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-function click()
+//-- Constants --\\
+
+const vid = document.querySelector("[data-vidTemplate]")
+const main1Section = document.querySelector(".main1")
+const specialSection = document.querySelector(".special")
+const main2Section = document.querySelector(".main2")
+
+const main1VidCount = 5
+const specialVidCount = 5
+const main2VidCount = 100
+
+
+//-- features --\\
+
+for(let i = 0; i < main1VidCount; i++)
 {
-    console.log("click")
+    main1Section.appendChild(vid.content.cloneNode(true))
+}
+
+for(let i = 0; i < specialVidCount; i++)
+{
+    specialSection.appendChild(vid.content.cloneNode(true))
+}
+
+for(let i = 0; i < main2VidCount; i++)
+{
+    main2Section.appendChild(vid.content.cloneNode(true))
 }
